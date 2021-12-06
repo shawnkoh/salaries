c439ea569fcce039e062d4b16cf5bc6c85982158
Shawn Koh <shawn@shawnkoh.sg>
2021-12-06 23:16:11 +0800

    Remove old build script

--- build.js
+++ build.js
@@ -22,17 +22,3 @@ devServer.start(
 		onBeforeRebuild() {},
 	}
 )
-
-// build({
-// 	entryPoints: ['src/index.js'],
-// 	// Warning: bundle does not cleanup unused files.
-// 	bundle: true,
-// 	outdir: 'dist',
-// 	watch: process.argv.includes("--watch"),
-// 	plugins: [
-// 		ElmPlugin({
-// 			debug: true,
-// 			clearOnWatch: true
-// 		})
-// 	]
-// }).catch(e => (console.error(e), process.exit(1)))
